0128c62a9d34a6fd4708ac59dc8a058469bf77da
lucavella <luca.iacovella@protonmail.com>
2024-01-04 07:28:15 +0100

    default map env added

---
+++ src/model/init_map.py
@@ -0,0 +1,28 @@
+from .env_map import EnvMap
+
+
+
+DEFAULT_MAP = EnvMap([
+    "##########################################",
+    "#            o      ooooo           o    #",
+    "#        o   oo         ooo        o  o  #",
+    "#     o ooo  ooo    o    o  o ooo oooo   #",
+    "# o  ooo o    o  o ooo  o  ooo o   o o   #",
+    "#ooo  o o    o  ooo o  ooo  o o       o  #",
+    "# o o  ooo  ooo  o o    o o  ooo   oo oo #",
+    "#  ooo  o    o o  ooo    ooo  o     ooo  #",
+    "#   o         ooo  o      o          o   #",
+    "#  ooo    o    o               o    ooo  #",
+    "#   o  o ooo    o  o ooo      o  ooo o   #",
+    "#  o  ooo o    o  ooo o      ooo  o o    #",
+    "# ooo  o o    ooo  o o        o o  ooo   #",
+    "#  o o  ooo    o o  ooo        ooo  o    #",
+    "#   ooo  o      ooo  o    o  o ooo       #",
+    "#    o       o  o ooo    o  ooo o        #",
+    "#o  ooo  o  o  ooo o    ooo  o o         #",
+    "#    o o   ooo  o o      o o  ooo        #",
+    "#     ooo   o o  ooo      ooo  o         #",
+    "# o    o     ooo  o        o             #",
+    "#             o                          #",
+    "##########################################",
+], [((16, 34), (21, 41))])
